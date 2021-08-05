241fe484fcaa8051477702d53d7319f420056f16
R.Doneux <robertdoneux@gmail.com>
2021-08-05 17:56:28 +0100

    adds main application test

---
+++ src/test/java/com/aswebsiteapi/AsWebsiteApiApplicationTest.java
@@ -0,0 +1,31 @@
+package com.aswebsiteapi;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.web.server.LocalManagementPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AsWebsiteApiApplicationTest {
+
+    @LocalManagementPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Test
+    void testHealthEndpointRouting() throws Exception {
+        var response = template.getForEntity("http://localhost:" + port + "/actuator/health", String.class);
+
+        assertThat(response.getStatusCodeValue()).isEqualTo(200);
+        assertThat(response.getBody()).contains("UP");
+    }
+
+}
